be5834be18fbce82277b721f4fd4047c02762be3
Siphosihle Sehume <sisehjhb025@student.wethinkcode.co.za>
2026-04-18 16:06:46 +0200

    Added "Graceful Crash Logic" in case client sends malformed JSON

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import java.util.Scanner;
 
 public class Main {
@@ -9,31 +12,38 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ObjectMapper mapper = new ObjectMapper();
 
+        //Configure ObjectMapper first
+        //Graceful Error Handling; Don't crash if a client sends in a field your Request class doesn't have
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //Make the output pretty for your own debugging logs
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
         System.out.println("Enter a JSON string: ");
         String jsonLine = scanner.nextLine();
 
-        //Deserialize
-        Request request = mapper.readValue(jsonLine, Request.class);
-        System.out.println("Request command: " + request.getCommand());
+        try {
+            //Deserialize
+            Request request = mapper.readValue(jsonLine, Request.class);
+            System.out.println("Request command: " + request.getCommand());
 
-        //Mimicking Server logic
-        Robot robot = new Robot();
-        RobotState currentState = new RobotState();
+            //Mimicking Server logic
+            Robot robot = new Robot();
+            RobotState currentState = new RobotState();
 
-        Response response = new Response(
-                "OK",
-                robot,
-                currentState
-        );
+            Response response = new Response(
+                    "OK",
+                    robot,
+                    currentState
+            );
 
-        //Serialize Output
-        String jsonResponse = mapper.writerWithDefaultPrettyPrinter()
-                .writeValueAsString(response);
+            //Serialize Output
+            String jsonResponse = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(response);
 
-        System.out.println(jsonResponse);
-
-
-
+            System.out.println(jsonResponse);
+        } catch (JsonProcessingException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
 
     }
 }
